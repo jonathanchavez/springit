c98c9c5c08b539cc95cd39d23dc0c970d967c77e
jonathanchavez <jchavez2017@gmail.cim>
2021-01-04 15:25:35 -0500

    initial commit for spring boot reddit clone

---
+++ src/main/java/com/vega/sprinit/SpringitApplication.java
@@ -0,0 +1,15 @@
+package com.vega.sprinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //comprise or made up of other anotation
+public class SpringitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringitApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/vega/sprinit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vega.sprinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
