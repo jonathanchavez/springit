5a40deadc78b27e6baeb280ac9dcf2a742c3ea37
jonathanchavez <jchavez2017@gmail.cim>
2021-01-05 09:53:06 -0500

    created entitites and refactored using project lombok

---
+++ src/main/java/com/vega/sprinit/domain/Link.java
@@ -0,0 +1,26 @@
+package com.vega.sprinit.domain;
+//a class to a table model, represent a table in a database
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity   //specifies that this class is a entity
+//replace boilerplate code
+@NoArgsConstructor  //public constructor gets rid
+@Data //get rids of getters and setters, all togehter shortcut for getters,setters,tostring.equlas,hashcode
+public class Link {
+
+    @Id  //specifies the primary key of the entity
+    @GeneratedValue  //provides the specification of generation stragerty of primary keys
+    private Long id;
+    private String title;
+    private String url;
+
+
+}
