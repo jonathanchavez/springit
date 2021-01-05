98fd8c803dcab236fdaf262081c9ef75d57aacf6
jonathanchavez <jchavez2017@gmail.cim>
2021-01-05 16:26:56 -0500

    setting up database

--- src/main/java/com/vega/sprinit/repository/LinkRepository.java
+++ src/main/java/com/vega/sprinit/repository/LinkRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LinkRepository extends JpaRepository<Link,Long> //object and type of object
  {
+ Link findByTitle(String title);
+
+
 
 }
